062f0d10bfd6758a469228922fb40fceefc113b6
Eddy Beaupre <eddy@beaupre.biz>
2013-11-13 14:29:03 -0500

    Initial commit

---
+++ Decrypt7/Decrypt7.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Decrypt7
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Decrypt7))
+        Me.CryptBox1 = New System.Windows.Forms.TextBox()
+        Me.Start1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'CryptBox1
+        '
+        Me.CryptBox1.Location = New System.Drawing.Point(12, 25)
+        Me.CryptBox1.Name = "CryptBox1"
+        Me.CryptBox1.Size = New System.Drawing.Size(279, 20)
+        Me.CryptBox1.TabIndex = 0
+        '
+        'Start1
+        '
+        Me.Start1.Location = New System.Drawing.Point(12, 51)
+        Me.Start1.Name = "Start1"
+        Me.Start1.Size = New System.Drawing.Size(75, 23)
+        Me.Start1.TabIndex = 1
+        Me.Start1.Text = "Decrypt"
+        Me.Start1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.Location = New System.Drawing.Point(93, 54)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal
+        Me.TextBox2.Size = New System.Drawing.Size(198, 20)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.WordWrap = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(98, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Crypted Password :"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(304, 89)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Start1)
+        Me.Controls.Add(Me.CryptBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Password 7 Decrypter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents CryptBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Start1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Decrypt7/Decrypt7.vb
@@ -0,0 +1,31 @@
+﻿Public Class Decrypt7
+
+    Public Function Decrypt(Encrypted As String) As String
+        Dim XIdx As Integer
+        Dim Xlat() As Object = {&H64, &H73, &H66, &H64, &H3B, &H6B, &H66, &H6F, &H41, &H2C, &H2E, &H69, &H79, &H65, &H77, &H72, &H6B, &H6C, &H64, &H4A, &H4B, &H44, &H48, &H53, &H55, &H42, &H73, &H67, &H76, &H63, &H61, &H36, &H39, &H38, &H33, &H34, &H6E, &H63, &H78, &H76, &H39, &H38, &H37, &H33, &H32, &H35, &H34, &H6B, &H3B, &H66, &H67, &H38, &H37}
+
+        Encrypted = Trim(Encrypted)
+        Decrypt = vbNullString
+
+        If Encrypted.Length > 2 Then
+            XIdx = Val(Strings.Left(Encrypted, 2))
+            Encrypted = Strings.Right(Encrypted, Len(Encrypted) - 2)
+            Do
+                Decrypt = Decrypt & Chr(Val("&H" & Strings.Left(Encrypted, 2)) Xor Xlat(XIdx))
+                If Len(Encrypted) >= 2 Then
+                    XIdx = XIdx + 1
+                    Encrypted = Strings.Right(Encrypted, Len(Encrypted) - 2)
+                    If XIdx >= Xlat.Length Then
+                        XIdx = 0
+                    End If
+                End If
+            Loop Until (Len(Encrypted) < 2)
+        End If
+
+    End Function
+
+    Private Sub Start1_Click(sender As Object, e As EventArgs) Handles Start1.Click
+        TextBox2.Text = Decrypt(CryptBox1.Text)
+    End Sub
+
+End Class
