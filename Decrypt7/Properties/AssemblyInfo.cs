0e90adb6d19bcddd57a97fb7fd0d2ed60a725302
Eddy Beaupré <eddy@beaupre.biz>
2016-01-17 14:17:03 -0500

    Convert to Visual Studio 2015 / CSharp

---
+++ Decrypt7/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Decrypt7")]
+[assembly: AssemblyDescription("Decrypt Cisco's Password 7")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Eddy Beaupré")]
+[assembly: AssemblyProduct("Decrypt7")]
+[assembly: AssemblyCopyright("Copyright ©  2016 Eddy Beaupré")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2e673ed3-6ec1-4e17-ac72-673ae0b63099")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
